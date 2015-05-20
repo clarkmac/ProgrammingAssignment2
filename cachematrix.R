2a6ee477ebab3fe61b3952bddf3c2b8ee67a9807
clarkmac <clark@macdonaldusa.com>
2015-05-20 16:53:32 -0400

    second update
    testing github process  committing, cachematrix.r

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,22 @@
 ## The first function will create a specail "matrix" that can save its inverse
 ## to cache.  The second function computes the inverse of this special "matrix" 
-## and returns it, unless it's already been cached.   
-## functions do
+## and returns it, unless it's already been cached. 
 
-## Write a short comment describing this function
+## Write a short comment describing this function, sets m and y to Null
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      m <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      get <- function() x
+      setmatrix <- function(mean) m <<- mean
+      getmean <- function() m
+## Out put a list      
+      list(setmatrix = setmatrix, getmatrix = getmatrix,
+           setinverse = setinverse,
+           getinverse = getinverse)
 }
 
 
